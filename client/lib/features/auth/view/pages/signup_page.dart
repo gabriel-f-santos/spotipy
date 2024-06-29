f5f9b37489f9a08df3c772f956f7eaf36dacbde6
Gabriel <gabrielfgsantos95@gmail.com>
2024-06-29 15:42:08 -0300

    feat: finish signup page ui

--- client/lib/features/auth/view/pages/signup_page.dart
+++ client/lib/features/auth/view/pages/signup_page.dart
@@ -13,47 +13,67 @@ class SignupPage extends StatefulWidget {
 }
 
 class _SignupPageState extends State<SignupPage> {
+  final _nameController = TextEditingController();
+  final _emailController = TextEditingController();
+  final _passwordController = TextEditingController();
+  final _formKey = GlobalKey<FormState>();
+
+  @override
+  void dispose() {
+    _nameController.dispose();
+    _emailController.dispose();
+    _passwordController.dispose();
+    super.dispose();
+    _formKey.currentState?.validate();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(),
       body: Padding(
         padding: EdgeInsets.all(15.0),
+        child: Form(
+          key: _formKey,
         child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Text(
-              'Signup Page.',
-              style: TextStyle(
-                fontWeight: FontWeight.bold,
-                fontSize: 50,
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(
+                'Signup Page.',
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  fontSize: 50,
+                ),
               ),
-            ),
-            SizedBox(height: 30),
-            CustomField(hintText: 'Name'),
-            SizedBox(height: 15),
-            CustomField(hintText: 'Email'),
-            SizedBox(height: 15),
-            CustomField(hintText: 'Password'),
-            SizedBox(height: 20),
-            AuthGradientButton(),
-            SizedBox(height: 20),
-            RichText(
-              text: TextSpan(
-                text: 'Already have an account? ',
-                style: Theme.of(context).textTheme.titleMedium,
-                children: const [
-                  TextSpan(
-                    text: 'Sign in.',
-                    style: TextStyle(
-                      color: Pallete.gradient2,
-                      fontWeight: FontWeight.bold,
+              SizedBox(height: 30),
+              CustomField(hintText: 'Name', controller: _nameController),
+              SizedBox(height: 15),
+              CustomField(hintText: 'Email', controller: _emailController),
+              SizedBox(height: 15),
+              CustomField(
+                  hintText: 'Password',
+                  controller: _passwordController,
+                  isObscureText: true),
+              SizedBox(height: 20),
+              AuthGradientButton(),
+              SizedBox(height: 20),
+              RichText(
+                text: TextSpan(
+                  text: 'Already have an account? ',
+                  style: Theme.of(context).textTheme.titleMedium,
+                  children: const [
+                    TextSpan(
+                      text: 'Sign in.',
+                      style: TextStyle(
+                        color: Pallete.gradient2,
+                        fontWeight: FontWeight.bold,
+                      ),
                     ),
-                  ),
-                ],
+                  ],
+                ),
               ),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
--- client/lib/features/auth/view/widgets/custom_field.dart
+++ client/lib/features/auth/view/widgets/custom_field.dart
@@ -2,13 +2,27 @@ import 'package:flutter/material.dart';
 
 class CustomField extends StatelessWidget {
   final String hintText;
-  const CustomField({super.key, required this.hintText});
+  final TextEditingController controller;
+  final bool isObscureText;
+  const CustomField(
+      {super.key,
+      required this.hintText,
+      required this.controller,
+      this.isObscureText = false});
 
   @override
   Widget build(BuildContext context) {
     return TextFormField(
+        obscureText: isObscureText,
+        controller: controller,
+        validator: (value) {
+          if (value == null || value.trim().isEmpty) {
+            return '$hintText cannot be empty.';
+          }
+          return null;
+        },
         decoration: InputDecoration(
-      hintText: hintText,
-    ));
+          hintText: hintText,
+        ));
   }
 }
