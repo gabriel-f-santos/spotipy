9d9e1fac57c04a37bef0a3afa8574d9c9fbc271a
Gabriel <gabrielfgsantos95@gmail.com>
2024-06-30 18:18:33 -0300

    feat: use sqlite

--- server/src/config/settings.py
+++ server/src/config/settings.py
@@ -2,7 +2,7 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 
 
 class Settings(BaseSettings):
-    database_url: str = "sqlite+pysqlite:///:memory:"
+    database_url: str = "sqlite+pysqlite:///music.db"
     model_config = SettingsConfigDict(
         env_file=".env", env_file_encoding="utf-8"
     )
