fd6b369cf5af8b31fe4cdc94b99fa4a487a30786
Gabriel <gabrielfgsantos95@gmail.com>
2024-07-05 20:42:49 -0300

    feat: add picker imahe and audio

--- client/lib/core/utils.dart
+++ client/lib/core/utils.dart
@@ -1,6 +1,9 @@
 // import 'dart:io';
 
 // import 'package:file_picker/file_picker.dart';
+import 'dart:io';
+
+import 'package:file_picker/file_picker.dart';
 import 'package:flutter/material.dart';
 
 String rgbToHex(Color color) {
@@ -21,32 +24,32 @@ void showSnackBar(BuildContext context, String content) {
     );
 }
 
-// Future<File?> pickImage() async {
-//   try {
-//     final filePickerRes = await FilePicker.platform.pickFiles(
-//       type: FileType.image,
-//     );
+Future<File?> pickImage() async {
+  try {
+    final filePickerRes = await FilePicker.platform.pickFiles(
+      type: FileType.image,
+    );
 
-//     if (filePickerRes != null) {
-//       return File(filePickerRes.files.first.xFile.path);
-//     }
-//     return null;
-//   } catch (e) {
-//     return null;
-//   }
-// }
+    if (filePickerRes != null) {
+      return File(filePickerRes.files.first.xFile.path);
+    }
+    return null;
+  } catch (e) {
+    return null;
+  }
+}
 
-// Future<File?> pickAudio() async {
-//   try {
-//     final filePickerRes = await FilePicker.platform.pickFiles(
-//       type: FileType.audio,
-//     );
+Future<File?> pickAudio() async {
+  try {
+    final filePickerRes = await FilePicker.platform.pickFiles(
+      type: FileType.audio,
+    );
 
-//     if (filePickerRes != null) {
-//       return File(filePickerRes.files.first.xFile.path);
-//     }
-//     return null;
-//   } catch (e) {
-//     return null;
-//   }
-// }
+    if (filePickerRes != null) {
+      return File(filePickerRes.files.first.xFile.path);
+    }
+    return null;
+  } catch (e) {
+    return null;
+  }
+}
--- client/lib/features/home/view/pages/upload_song_page.dart
+++ client/lib/features/home/view/pages/upload_song_page.dart
@@ -1,6 +1,10 @@
+import 'dart:io';
+
 import 'package:client/core/theme/app_pallete.dart';
+import 'package:client/core/utils.dart';
 import 'package:client/core/widgets/custom_field.dart';
 import 'package:dotted_border/dotted_border.dart';
+import 'package:flex_color_picker/flex_color_picker.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
@@ -14,6 +18,27 @@ class UploadSongPage extends ConsumerStatefulWidget {
 class _UploadSongPageState extends ConsumerState<UploadSongPage> {
   final _songNameController = TextEditingController();
   final _artistNameController = TextEditingController();
+  Color _selectedColor = Pallete.cardColor;
+  File? selectedAudio;
+  File? selectedImage;
+
+  void selecteAudio() async {
+    final audio = await pickAudio();
+    if (audio != null) {
+      setState(() {
+        selectedAudio = audio;
+      });
+    }
+  }
+
+  void selectImage() async {
+    final image = await pickImage();
+    if (image != null) {
+      setState(() {
+        selectedImage = image;
+      });
+    }
+  }
 
   @override
   void dispose() {
@@ -27,41 +52,75 @@ class _UploadSongPageState extends ConsumerState<UploadSongPage> {
     return Scaffold(
       appBar: AppBar(
         title: const Text('Upload Song'),
+        actions: [
+          IconButton(
+            onPressed: () async {
+              // if (
+              //   formKey.currentState!.validate() &&
+              //     selectedAudio != null &&
+              //     selectedImage != null)
+              //     {
+              //   ref.read(homeViewModelProvider.notifier).uploadSong(
+              //         selectedAudio: selectedAudio!,
+              //         selectedThumbnail: selectedImage!,
+              //         songName: songNameController.text,
+              //         artist: artistController.text,
+              //         selectedColor: selectedColor,
+              //       );
+              // } else {
+              //   showSnackBar(context, 'Missing fields!');
+              // }
+            },
+            icon: const Icon(Icons.check),
+          ),
+        ],
       ),
-      body: Padding(
-        padding: const EdgeInsets.all(20.0),
-        child: SingleChildScrollView(
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.all(20.0),
           child: Column(
             children: [
-              DottedBorder(
-                borderType: BorderType.RRect,
-                color: Pallete.borderColor,
-                strokeCap: StrokeCap.round,
-                dashPattern: const [10, 4],
-                child: const SizedBox(
-                  width: double.infinity,
-                  height: 150,
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Icon(
-                        Icons.folder_open,
-                        size: 40,
+              GestureDetector(
+                onTap: selectImage,
+                child: selectedImage != null
+                    ? SizedBox(
+                        width: double.infinity,
+                        height: 150,
+                        child: Image.file(
+                          selectedImage!,
+                          fit: BoxFit.cover,
+                        ),
+                      )
+                    : DottedBorder(
+                        borderType: BorderType.RRect,
+                        color: Pallete.borderColor,
+                        strokeCap: StrokeCap.round,
+                        dashPattern: const [10, 4],
+                        child: const SizedBox(
+                          width: double.infinity,
+                          height: 150,
+                          child: Column(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              Icon(
+                                Icons.folder_open,
+                                size: 40,
+                              ),
+                              SizedBox(height: 20),
+                              Text(
+                                'Select your thumbnail',
+                              ),
+                            ],
+                          ),
+                        ),
                       ),
-                      SizedBox(height: 10),
-                      Text(
-                        'Drag and drop your song here',
-                      ),
-                    ],
-                  ),
-                ),
               ),
               const SizedBox(height: 20),
               CustomField(
                 hintText: "Pick Song",
                 readOnly: true,
                 controller: null,
-                onTap: () {},
+                onTap: selecteAudio,
               ),
               const SizedBox(height: 20),
               CustomField(
@@ -73,6 +132,18 @@ class _UploadSongPageState extends ConsumerState<UploadSongPage> {
                 hintText: "Song Name",
                 controller: _songNameController,
               ),
+              const SizedBox(height: 20),
+              ColorPicker(
+                pickersEnabled: const {
+                  ColorPickerType.wheel: true,
+                },
+                color: _selectedColor,
+                onColorChanged: (Color color) {
+                  setState(() {
+                    _selectedColor = color;
+                  });
+                },
+              )
             ],
           ),
         ),
